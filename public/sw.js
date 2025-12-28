fb5ffd7f43568ad6b31116c63df6811d5591bb0f
Andyking1-pixel <jordan.alvarez@academia-ciberseguridad.com>
2025-12-28 15:19:19 -0600

    fix: actualizar sw cache y frontend

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Versión del caché (puedes subirla cuando hagas cambios grandes)
-const CACHE_NAME = "msf-cache-v3";
+const CACHE_NAME = "msf-cache-v5";
 
 // Archivos base que queremos tener cacheados para offline
 const ASSETS = [
